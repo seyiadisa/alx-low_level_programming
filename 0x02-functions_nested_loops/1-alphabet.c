00c55c4fc33ad9af17d5f25666585de99a6c3bbf
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-02-21 22:50:09 +0100

    completed task 1 of c functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+int _putchar(char);
+
+/**
+  * print_alphabet - prints all the letters of the alphabet
+  * in lowercase
+  */
+void print_alphabet(void)
+{
+	for (int i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar('\n');
+}
