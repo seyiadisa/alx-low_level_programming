7dd8171c2f274a01be8c91b7b897366b1299e87b
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-02-22 03:25:34 +0100

    fixed errors with c functions project

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,6 +7,7 @@
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i < 123; i++)
 		_putchar(i);
 	_putchar('\n');
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,40 +7,41 @@ void times_table(void)
 {
 	int col, row, product, ten, one;
 
-	for (col = 0; col < 10; col++)
+	for (row = 0; row < 10; row++)
 	{
-		for (row = 0; row < 10; row++)
+		for (col = 0; col < 10; col++)
 		{
 			product = col * row;
 			ten = product / 10;
 			one = product % 10;
 
-			if ((product < 10) && (row != 9))
+			if (col == 0)
 			{
+                                if (product < 10)
+                                {
+                                        _putchar(product + '0');
+                                }
+                                else
+                                {
+                                        _putchar(ten + '0');
+                                        _putchar(one + '0');
+                                }
+                                continue;
+                        }
+
+			if (product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(product + '0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else if ((product >= 10) && (row != 9))
-			{
-				_putchar(ten + '0');
-				_putchar(one + '0');
-				_putchar(',');
-				_putchar(' ');
 			}
 			else
 			{
-				if (product < 10)
-				{
-					_putchar(product + '0');
-				}
-				else
-				{
-					_putchar(ten + '0');
-					_putchar(one + '0');
-				}
-				continue;
+				_putchar(',');
+				_putchar(' ');
+				_putchar(ten + '0');
+                                _putchar(one + '0');
 			}
 		}
 		_putchar('\n');
