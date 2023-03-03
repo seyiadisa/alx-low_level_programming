46bb8e4f75604f806ab3b9a0b0ef915c7be350d4
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-03-03 04:26:03 +0100

    fixed errors

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,23 +4,21 @@
   * leet - a function that encodes a string into 1337 (leet)
   * @str: string to encode
   *
-  * Return: encoded string
+  * Return: a pointer to the encoded string
   */
 char *leet(char *str)
 {
 	int i, j;
-	int len = 0;
-	char letters[5] = {'a', 'e', 'o', 't', 'l'};
-	int encode[5] = {4, 3, 0, 7, 1};
+	char encode[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
 		{
-			if (str[i] == letters[j] ||
-					str[i] == letters[j] - 32)
+			if (str[i] == encode[j] ||
+					str[i] - 32 == encode[j])
 			{
-				str[i] = encode[j] + '0';
+				str[i] = j + '0';
 			}
 		}
 	}
