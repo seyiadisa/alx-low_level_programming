f8847ca834642214deec531c5006723930fe4a3b
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-03-29 02:15:53 +0100

    Completed advanced tasks for C - hello, world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - program execution begins here
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+
 	return (0);
 }
--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - program execution begins here
+ *
+ * Return: 0 if successful
+ */
 int main(void)
 {
-		puts("Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
