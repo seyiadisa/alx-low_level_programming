1ac1edd382f05c253edff0797badc01eff7450b7
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-04-05 10:20:14 +0100

    fixed 3-alloc_grid.c to return NULL if malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,6 +17,9 @@ int **alloc_grid(int width, int height)
 
 	ptr = (int **)malloc(height * sizeof(int *));
 
+	if (ptr == NULL)
+		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
 		ptr[i] = (int *)malloc(width * sizeof(int));
