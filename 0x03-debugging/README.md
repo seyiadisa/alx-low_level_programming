11a1c0b6c351c1833860fca4199d573be60cb35b
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-11-02 17:52:31 +0100

    complete tasks for C - Debugging

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-This file contains my files for the ALX SE project: C - Debugging
+# C - Debugging
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,9 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int);
-
 int largest_number(int, int, int);
-
 int convert_day(int, int);
+void print_remaining_days(int month, int day, int year);
 
 #endif
