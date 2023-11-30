02909390e519eaf254bbc57f1c88f5b94eb8f686
Oluwaseyi Adisa <oluwaseyiadisaa@gmail.com>
2023-11-30 02:00:23 +0100

    complete advanced task for C - function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - a function that executes a function given as a parameter on each element of the array
+ * array_iterator - a function that executes a function given as a parameter
+ *	on each element of the array
  * @array: array of elements
  * @size: size of the array
  * @action: pointer to function to run on each element of @array
